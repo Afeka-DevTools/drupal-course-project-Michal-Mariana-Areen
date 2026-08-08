1928ca7172481606d6d2f2880d859a1d9b303f4e
michalgabovich1-boop <Michal.Gabovich@s.afeka.ac.il>
2026-08-08 17:21:54 +0300

    Update restore script for Drupal volumes restoration

--- restore.sh
+++ restore.sh
@@ -12,7 +12,9 @@ echo "[2/4] Restoring PostgreSQL database..."
 docker cp drupal_db_backup.sql postgres-db:/drupal_db_backup.sql
 docker exec postgres-db psql -U root -d drupal_db -f /drupal_db_backup.sql
 
-echo "[3/4] Restoring Drupal Volumes (Design and Files - FIXED PATH)..."
-docker exec -i drupal-app tar -xzf - -C /opt/drupal/web < drupal_volumes_backup.tar.gz
+echo "[3/4] Restoring Drupal Volumes..."
+docker cp drupal_volumes_backup.tar.gz drupal-app:/opt/drupal/drupal_volumes_backup.tar.gz
+docker exec drupal-app tar -xzf /opt/drupal/drupal_volumes_backup.tar.gz -C /opt/drupal/web
+docker exec drupal-app rm /opt/drupal/drupal_volumes_backup.tar.gz
 
 echo "=== Restore Completed Successfully! ==="
