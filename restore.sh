1d07a2a57347fd78a7dccdb83b2a894a19d0ec87
michalgabovich1-boop <Michal.Gabovich@s.afeka.ac.il>
2026-08-08 15:58:58 +0300

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -5,12 +5,12 @@ echo "[1/4] Setting up Docker containers and network..."
 chmod +x setup.sh
 ./setup.sh
 
-echo "Waiting 10 seconds for PostgreSQL and Drupal to initialize..."
-sleep 10
+echo "Waiting 20 seconds for PostgreSQL and Drupal to initialize..."
+sleep 20
 
 echo "[2/4] Restoring PostgreSQL database..."
 docker cp drupal_db_backup.sql postgres-db:/drupal_db_backup.sql
-docker exec postgres-db psql -U postgres -d postgres -f /drupal_db_backup.sql
+docker exec postgres-db psql -U root -d drupal_db -f /drupal_db_backup.sql
 
 echo "[3/4] Restoring Drupal Volumes (Design and Files - FIXED PATH)..."
 docker exec -i drupal-app tar -xzf - -C /opt/drupal/web < drupal_volumes_backup.tar.gz
