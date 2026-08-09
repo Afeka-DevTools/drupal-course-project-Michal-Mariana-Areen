f967d31d977a2ea2d15d53ed6e141b9e89a56edb
michalgabovich1-boop <Michal.Gabovich@s.afeka.ac.il>
2026-08-09 18:54:47 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,20 @@
+<?php
+$settings['hash_salt'] = 'afeka-course-secret-key-123456';
+$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+$settings['entity_update_batch_size'] = 50;
+$settings['entity_update_backup'] = TRUE;
+$settings['migrate_node_migrate_type_default'] = 'idle';
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'host' => 'drupal-db',
+  'port' => '3306',
+  'driver' => 'mysql',
+);
